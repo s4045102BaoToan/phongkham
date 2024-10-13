581872514cd16ef96e374dc38aa445a3ab841726
Bao Toan <s4045102@rmit.edu.vn>
2024-10-13 12:47:10 +0700

    apllied loading screen to index.html

--- public/javascript/loadingscreen.js
+++ public/javascript/loadingscreen.js
@@ -1,10 +1,25 @@
 window.addEventListener('load', function() {
     const loadingScreen = document.querySelector('#loadingScreen');
     const content = document.querySelector('.all-content');
-    
-    // Hide the loading screen
-    loadingScreen.style.display = 'none';
+    const spinner = document.querySelector('#loadingAnimation');
+    const logo = document.querySelector('#loadingImg');
+    const left = document.querySelector('#left');
+    const right = document.querySelector('#right');
+    //Play loading animation when loaded
+    setTimeout(() => {
+        console.log('Setting timeout for some dramtic effect lol');
+        loadingScreen.classList.add('bg-fade-out');
+        logo.classList.add('logo-fade-out');
+        spinner.classList.add('logo-fade-out');
+        left.classList.add('slide-to-left');
+        right.classList.add('slide-to-right');
+    }, 3000);
+    setTimeout(() => {
+        loadingScreen.style.display = "none";
+    }, 4000);
     
     // Show the main content
-    content.style.display = 'block';
+    setTimeout(() => {
+        content.style.display = 'block';
+    }, 3000);
 });
