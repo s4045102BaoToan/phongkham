fcd781a305cd13fe960009d55100385a1a8d037e
Bao Toan <s4045102@rmit.edu.vn>
2024-10-21 22:29:35 +0700

    fixed nav bar

--- public/javascript/paralax.js
+++ public/javascript/paralax.js
@@ -3,6 +3,6 @@ const parallax = document.querySelector(".parallax");
 // Parallax Effect for DIV 1
 window.addEventListener("scroll", function () {
   let offset = window.pageYOffset;
-  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+  parallax.style.backgroundPositionY = offset * 0.3 + "px";
   // DIV 1 background will move slower than other elements on scroll.
 });
